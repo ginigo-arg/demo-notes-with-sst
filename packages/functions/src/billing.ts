110761c7f810d5bace06f0b2054f14bd431a2bf3
ginigo-arg <gabrielinigo99@gmail.com>
2024-05-07 12:08:12 -0300

    Adding billing API

---
+++ packages/functions/src/billing.ts
@@ -0,0 +1,25 @@
+import Stripe from "stripe";
+import { Config } from "sst/node/config";
+import handler from "@notes/core/handler";
+import { calculateCost } from "@notes/core/cost";
+
+export const main = handler(async (event) => {
+  const { storage, source } = JSON.parse(event.body || "{}");
+  const amount = calculateCost(storage);
+  console.log("amount", amount);
+  const description = "Scratch charge";
+
+  // Load our secret key
+  const stripe = new Stripe(Config.STRIPE_SECRET_KEY, {
+    apiVersion: "2024-04-10",
+  });
+
+  await stripe.charges.create({
+    source,
+    amount,
+    description,
+    currency: "usd",
+  });
+
+  return JSON.stringify({ status: true });
+});
