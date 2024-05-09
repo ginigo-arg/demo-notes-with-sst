fb9c168f838a654edd7777192b0b50bd36471f4f
ginigo-arg <gabrielinigo99@gmail.com>
2024-05-08 21:06:14 -0300

    wip: setting shadcn

--- packages/frontend/src/app/page.tsx
+++ packages/frontend/src/app/page.tsx
@@ -1,5 +1,9 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
-  return <>hola</>;
+  return (
+    <div>
+      <Button>Click me</Button>
+    </div>
+  );
 }
